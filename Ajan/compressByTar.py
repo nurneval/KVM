9d99f93a97b15140058c6f5c017ef015ef29608e
test <nurneval@gmail.com>
2019-06-25 16:23:54 +0300

    commit2
    	new file:   Ajan/compressByTar.py
    	new file:   Ajan/memoryUsage.py
    	new file:   Ajan/testing.py

---
+++ Ajan/compressByTar.py
@@ -0,0 +1,22 @@
+
+import tarfile
+
+import sys
+
+
+def compress( fileNameToBeCompressed,compressedFileName, compressionMode  ):
+    print 'creating archive:' + compressedFileName
+    out = tarfile.open(compressedFileName, mode='w:gz')
+    try:
+	    print 'adding ' + fileNameToBeCompressed
+	    out.add(sys.argv[1])
+    finally:
+        print 'closing ' + compressedFileName
+        out.close()
+
+	print
+	print 'Contents added to '  + compressedFileName +' :'
+	t = tarfile.open(compressedFileName, 'r')
+	for member_info in t.getmembers():
+	    print member_info.name
+    return;
