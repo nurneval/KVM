8c9c0866c0bfd5458c0d3fae76eab594052c2054
test <nurneval@gmail.com>
2019-06-25 15:09:50 +0300

    Initial commit 	new file:   Ajan/compressByTar.py 	new file:   Ajan/memoryUsage.py 	new file:   Ajan/testing.py

---
+++ Ajan/memoryUsage.py
@@ -0,0 +1,9 @@
+import resource
+def using(point=""):
+    usage=resource.getrusage(resource.RUSAGE_SELF)
+    return '''%s: usertime=%s systime=%s mem=%s mb
+           '''%(point,usage[0],usage[1],
+                (usage[2]*resource.getpagesize())/1000000.0 )
+
+
+#print using("current usage")
