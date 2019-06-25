010a02bfca69dcb043fc026594fa61a02ea07518
test <nurneval@gmail.com>
2019-06-25 16:00:46 +0300

    commit 1 	deleted:    KVM/Ajan/compressByTar.py 	deleted:    KVM/Ajan/memoryUsage.py 	deleted:    KVM/Ajan/testing.py 	deleted:    README.md

--- KVM/Ajan/memoryUsage.py
+++
@@ -1,9 +0,0 @@
-import resource
-def using(point=""):
-    usage=resource.getrusage(resource.RUSAGE_SELF)
-    return '''%s: usertime=%s systime=%s mem=%s mb
-           '''%(point,usage[0],usage[1],
-                (usage[2]*resource.getpagesize())/1000000.0 )
-
-
-print using("current usage")
