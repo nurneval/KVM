71a76488e251d231fb2612bacbac8df60c29d1eb
Gulnur Neval Erdem <nurneval@gmail.com>
2019-07-11 19:01:58 +0300

    code optimizasyonu yapmadan çalıştım.  gereksiz dosyların silinmesi

--- Testing/compressionTests/memoryUsage.py
+++
@@ -1,8 +0,0 @@
-import resource
-def using():
-    usage=resource.getrusage(resource.RUSAGE_SELF)
-    return '''usertime=%s systime=%s mem=%s mb '''%(usage[0],usage[1],
-                (usage[2]*resource.getpagesize())/1000000.0 )
-
-
- #print using("current usage")
